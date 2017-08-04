9e54af4e1327ec62471c264ef2adf1513570b069
David Harillo Sánchez <dhs.seck@gmail.com>
2017-08-04 22:42:41 +0200

    Add mongo. Create mongoUtil module to connect with the database.

--- server/app.js
+++ server/app.js
@@ -3,10 +3,17 @@
 let express = require('express');
 let app = express();
 
+let mongoUtil = require('./mongoUtil');
+mongoUtil.connect();
+
 app.use( express.static(__dirname + '/../client'));
 
 app.get('/sports', (request, response) => {
-    response.json(['Weightlifting', 'Ciclying']);
+  let sports = mongoUtil.sports();
+  sports.find().toArray((err, docs) => {
+    const sports = docs.map((sport) => sport.name);
+    response.json(sports);
+  });
 });
 
 app.listen(8181, () =>  console.log('Listening on 8181...'));
